aec51d7cf3a8761a2c66d294bb068eee9e3294b6
minhtrang160199 <ntmtrang160199@gmail.com>
2023-06-07 21:52:35 +0100

    Commit all

--- npl.py
+++ npl.py
@@ -3,7 +3,7 @@ import pandas as pd
 df = pd.read_csv('/Users/mimi/binance_reviews.txt', sep="\n"*2, header=None)
 print(df)
 df = df.rename(columns={0: 'review'})
-print(df)
+
 
 # import library
 import nltk
@@ -14,6 +14,7 @@ from nltk.tokenize import sent_tokenize
 from nltk.sentiment import SentimentIntensityAnalyzer
 from nltk.stem.porter import PorterStemmer
 from nltk.stem import WordNetLemmatizer
+import re
 
 # Download NLTK resources (only needed once)
 nltk.download('punkt')
@@ -24,11 +25,27 @@ nltk.download('tagsets')
 nltk.download('wordnet')
 nltk.download('omw-1.4')
 
+# Data cleaning
+# Remove special characters, symbols, numbers and combinations of letters and numbers
+df['clean_text'] = df['review'].apply(lambda x: re.sub(r'[^A-Za-z\s]', '', x))
+# Convert text to lowercase
+df['clean_text'] = df['clean_text'].str.lower()
+print(df['clean_text'].head(5))
+
+# Check for unwanted patterns 
+unwanted_patterns = ['@', '#', 'http', 'www']
+unwanted_data = df[df['clean_text'].str.contains('|'.join(unwanted_patterns))]
+print(unwanted_data)
+
+# Replace unwanted patterns with an empty string 
+df['clean_text'] = df['clean_text'].replace('|'.join(unwanted_patterns), '', regex=True)
+print(df['clean_text'].iloc[1988])
+
 # tokenization
-df['word_review'] = df['review'].apply(lambda x: word_tokenize(x.lower()))
-df['sent_review'] = df['review'].apply(lambda x: sent_tokenize(x.lower()))
+# df['word_review'] = df['review'].apply(lambda x: word_tokenize(x.lower()))
 
 # removing stop words
+
 
 
 
